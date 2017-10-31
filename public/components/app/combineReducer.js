d58906c0d7a9561b697b2df24e12fb10c0ddb1ed
suiffet guillaume <suiffet.g@gmail.com>
2017-10-31 14:35:41 +0100

    conflict combine reducer

--- public/components/app/combineReducer.js
+++ public/components/app/combineReducer.js
@@ -1,11 +1,9 @@
 var combineReducers = require("redux").combineReducers;
 var formReducer = require('redux-form').reducer;
-var folder   = require('../../reducer/folderreducer');
 var folderSelected   = require('../../reducer/folderselectedreducer');
 var snippet   = require('../../reducer/snippetreducer');
 var snippetSelected   = require('../../reducer/snipselectedreducer');
 
 var usersdata = require("../../reducer/usersdatareducer");
-var globalReducers = combineReducers({  form: formReducer, folder, folderSelected, snippet, snippetSelected, usersdata });
+var globalReducers = combineReducers({  form: formReducer, folderSelected, snippet, snippetSelected, usersdata });
 module.exports = globalReducers;
-
