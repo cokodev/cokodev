97f8cd5aaba2e1fab5a3c9ef91e5487ef9abbd4f
suiffet guillaume <suiffet.g@gmail.com>
2017-11-14 18:22:40 +0100

    paraphe username

--- public/components/shared/listfoldershared.js
+++ public/components/shared/listfoldershared.js
@@ -41,7 +41,7 @@ class Listfoldershared extends React.Component {
                                 <div id="fo-folder" className={className}>
                                     <div>
                                         <a href="#">
-                                            <span className="userShared"></span>
+                                            <span className="userShared">{parapheToUppercase}</span>
                                             <i className="fa fa-folder sharedfolder" aria-hidden="true"></i>
                                         </a>
                                     </div>
