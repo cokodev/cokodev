68393ef608a47379d04be7aed43905e17852aa80
Olivia Tastet <otastet@yahoo.com>
2017-11-17 20:43:33 +0100

    error foldeselected reducer

--- public/components/app/combineReducer.js
+++ public/components/app/combineReducer.js
@@ -3,7 +3,7 @@ var formReducer = require('redux-form').reducer;
 var snippetSelected   = require('../../reducer/snipselectedreducer');
 var data = require("../../reducer/datareducer");
 var usersdata = require("../../reducer/usersdatareducer");
-var folderSelected = require("../../reducer/folderSelectedreducer");
+var folderSelected = require("../../reducer/folderselectedreducer");
 
 var globalReducers = combineReducers({  form: formReducer, folderSelected, snippetSelected, usersdata, data});
 module.exports = globalReducers;
